2fc92cb9ed07cdae53115a3618b16e9740c1a7f4
sugamasao <sugamasao@gmail.com>
2012-10-03 06:34:13 +0900

    created ap command.

--- lib/ap_command.rb
+++ lib/ap_command.rb
@@ -1,5 +1,48 @@
-require "ap_command/version"
+# encoding:utf-8
+
+require 'json'
+
+require 'awesome_print'
+require_relative "ap_command/version"
 
 module ApCommand
-  # Your code goes here...
+  class Application
+    def run(argv)
+      path = argv.first.to_s
+      version if(path == '--version' or path == '-v')
+      usage if(path == '--help' or path == '-h')
+      usage unless(File.exists?(path))
+      
+      begin
+        awesome(JSON.parse(File.read(path)))
+      rescue JSON::ParserError => e 
+        puts "Error: #{e.class} message => #{e.message}"
+        puts
+        usage
+      end
+
+    end
+
+    def awesome(data)
+      ap(data, indent: 2)
+    end
+
+    def usage
+      puts "usage:"
+      puts "%#{File.basename($0)} /path/to/jsonfile"
+      puts "json file awsome print for command line tool."
+      exit 
+    end
+
+    def version
+      puts "#{File.basename($0)} #{VERSION}"
+      exit
+    end
+
+
+    def self.invoke(argv)
+      self.new.run(argv)
+    end
+  end
 end
+
---
+++ spec/ap_command_spec.rb
@@ -0,0 +1,74 @@
+# encoding:utf-8
+require 'spec_helper'
+require 'tempfile'
+
+module ApCommand
+  describe Application do
+    context 'jsonファイルを受け取る' do
+      describe '.invoke' do
+        before do
+          @file = Tempfile.new('ap')
+          @file.puts({hoge:[1, 2, '3']}.to_json)
+          @file.close
+        end
+
+        it { Application.invoke([@file.path]) }
+
+        after do
+          @file.unlink
+        end
+      end
+    end
+
+    context 'jsonファイルが壊れている' do
+      describe '.invoke' do
+        before do
+          @file = Tempfile.new('ap')
+          @file.puts '{"hoge":[}'
+          @file.close
+        end
+
+        it { expect {Application.invoke([@file.path])}.to raise_error(SystemExit) }
+
+        after do
+          @file.unlink
+        end
+      end
+    end
+
+    context 'ファイルが存在しない' do
+      describe '.invoke' do
+        specify 'ヘルプが出力される' do
+          expect { Application.invoke(['/tmp/hoge/fuga/awesome']) }.to raise_error(SystemExit)
+        end
+      end
+    end
+
+    context '引数を省略された' do
+      describe '.invoke' do
+        specify 'ヘルプが出力される' do
+          expect { Application.invoke([]) }.to raise_error(SystemExit)
+        end
+      end
+    end
+
+    context 'ヘルプコマンドを入力された' do
+      describe '.invoke' do
+        specify 'ヘルプが出力される' do
+          expect { Application.invoke(['--help']) }.to raise_error(SystemExit)
+          expect { Application.invoke(['-h']) }.to raise_error(SystemExit)
+        end
+      end
+    end
+
+    context 'versionを入力された' do
+      describe '.invoke' do
+        specify 'versionが出力される' do
+          expect { Application.invoke(['--version']) }.to raise_error(SystemExit)
+          expect { Application.invoke(['-v']) }.to raise_error(SystemExit)
+        end
+      end
+    end
+
+  end
+end
