302f7a7a17591f000a6ab491a82d981dd5b5099f
sugamasao <sugamasao@gmail.com>
2012-10-03 23:09:20 +0900

    add comment. fix #1

--- lib/ap_command.rb
+++ lib/ap_command.rb
@@ -5,8 +5,17 @@ require 'json'
 require 'awesome_print'
 require_relative "ap_command/version"
 
+# Awesome Print Command Line Tool.
+# @author sugamasao
 module ApCommand
+
+  # ap_command main class.
   class Application
+
+    # execute application.
+    #
+    # @param [Array] argv ARGV object.
+    # @return [void]
     def run(argv)
       path = argv.first.to_s
       version if(path == '--version' or path == '-v')
@@ -20,13 +29,19 @@ module ApCommand
         puts
         usage
       end
-
     end
 
+    # call awesome_print.
+    #
+    # @param [Hash] data awesome print object.
+    # @return [void]
     def awesome(data)
       ap(data, indent: 2)
     end
 
+    # show usage.
+    #
+    # @return [void]
     def usage
       puts "usage:"
       puts "%#{File.basename($0)} /path/to/jsonfile"
@@ -34,12 +49,19 @@ module ApCommand
       exit 
     end
 
+    # show version.
+    #
+    # @return [void]
     def version
       puts "#{File.basename($0)} #{VERSION}"
       exit
     end
 
 
+    # execute application shortcut.
+    #
+    # @param [Array] argv ARGV object.
+    # @return [void]
     def self.invoke(argv)
       self.new.run(argv)
     end
--- lib/ap_command/version.rb
+++ lib/ap_command/version.rb
@@ -1,3 +1,4 @@
 module ApCommand
-  VERSION = "0.0.1"
+  # version
+  VERSION = "0.0.2"
 end
